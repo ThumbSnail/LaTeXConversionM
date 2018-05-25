e45be2f7958708fe36ff0c16918ab88e864dbf1e
ThumbSnail <thumbsnail@zoho.com>
2018-05-24 18:08:45 -0700

    Changed algo to better handle wrapping things in \text{}

--- js/main.js
+++ js/main.js
@@ -49,6 +49,76 @@ function convertFractiontoLatex(str) {
   return str;
 }
 
+//https://stackoverflow.com/questions/273789/is-there-a-version-of-javascripts-string-indexof-that-allows-for-regular-expr
+String.prototype.regexIndexOf = function(regex, startpos) {
+    var indexOf = this.substring(startpos || 0).search(regex);
+    return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
+}
+
+function getRidOfItalics(string) {
+  let arrayOfLines = string.split(/[\n\r]/g);
+  const regExp = /[a-zA-Z]/;
+  const notALetterRegExp = /[^a-zA-Z\.]/;
+
+  arrayOfLines.forEach(function(str, index) {
+    //protection against infinite loop:
+    let counter = 0;
+    //find a letter
+    let letterIndex = str.regexIndexOf(regExp, 0);
+    let posInString = letterIndex;
+    while (letterIndex >= 0) {
+
+      //if not preceded by \  (ie, if not a LaTeX keyword)
+      if (letterIndex == 0 || str.charAt(letterIndex - 1) != '\\') {
+        let indexStartWord = letterIndex;  //where to place \text{
+
+        str = str.substring(0, indexStartWord) + '\\text{' + str.substring(indexStartWord);
+        posInString += 7;  //move the position up the length of the newly added '\text{' plus the letter you found
+
+        //find end of word to add a brace:
+        let indexEndWord = str.regexIndexOf(notALetterRegExp, posInString);
+        if (indexEndWord < 0) {
+          str = str.substring(0) + '}';
+        }
+        else {
+          str = str.substring(0, indexEndWord) + '}' + str.substring(indexEndWord);
+        }
+        posInString = indexEndWord;
+
+        arrayOfLines[index] = str;
+      }
+      else { //jump to end of latex keyword to resume search (ie, find the next { or space for keywords like \times)
+        //if that keyword is \text, then jump to the CLOSING bracket:
+        if (str.substring(posInString, posInString + 4) == 'text') {
+          posInString = str.regexIndexOf(/[\}]/, posInString);
+        }
+        else {
+          posInString = str.regexIndexOf(/[\{\s]/, posInString);
+        }
+      }
+
+      //resume search for next potential letter:
+      if (posInString >= str.length || posInString == -1) {
+        letterIndex = -1;
+      }
+      else {
+        letterIndex = str.regexIndexOf(regExp, posInString);
+      }
+      posInString = letterIndex;
+
+      //just in case something unanticipated happens, kill the loop if it's run too much
+      counter++;
+      if (counter > 100) {
+        console.log('you stuck in infinite loop...');
+        break;
+      }
+    }
+    console.log(counter);
+  });
+
+  return arrayOfLines.join('\n');
+}
+
 function convertToDesiredHTML(str) {
   //replace * with multiplication symbol:
   str = str.replace(/\*/g, '\\times');
@@ -64,8 +134,19 @@ function convertToDesiredHTML(str) {
   //to remove italicizied text, wrap \text{} around any amount of letters that do NOT begin with a \
   //(?<=[\s0-9{(\[]|^) = if preceded by space, digit, {, ( or [  or the start of a line
   //([a-zA-Z]+) = any number of letters
-  let regExp = /(?<=[\s0-9{(\[]|^)([a-zA-Z]+)/g;
-  str = str.replace(regExp, '\\text{$&}');
+  //let regExp = /(?<=[\s0-9{(\[]|^)([a-zA-Z]+)/g;
+  //str = str.replace(regExp, '\\text{$&}');
+
+  //^That doesn't quite work as desired.  New idea:
+  //Find a letter
+  //As long as that letter is NOT preceeded by a \, it's a possible word not used as a LaTeX keyword
+    //^If it IS, then jump ahead until you find a {; anything beyond that has the potential to be a letter
+  //Thus, stick \text{ in front of it
+  //then travel until you find a NON-letter (digit/brace/symbol [EXCEPT a period]); place a } before it
+  str = getRidOfItalics(str);
+
+  //for me, I like a little more space around "vs.".  Thus, replace \text{vs.} with \text{ vs. }
+  str = str.replace(/vs\./g, ' vs. ');
 
   //to convert each line of LaTeX into the appropriate HTML
   let arrayOfLines = str.split(/[\n\r]/g);
@@ -88,18 +169,14 @@ function convertToDesiredHTML(str) {
 
 //load up an example
 (function() {
-  txtEntry.innerHTML = `a^2 + b^2 = c^2
-(2r^2) + (2r^2) = d^2
-4r^2 + 4r^2 = d^2
-8r^2 = d^2
-\\sqrt{8r^2} = \\sqrt{d^2}
-r\\sqrt{4 * 2} = d
-2r\\sqrt{2} \\text{ vs. } \\dfrac{5r}{2}
-
-(5)/(63)
+  txtEntry.innerHTML = `a^2 + b^2 = y
+\\sqrt{8r^2} = \\sqrt{d^2 * 4}
+2ab\\sqrt{2} vs. \\dfrac{5abc}{2d}
 distance = 5R * time
 
-(7x^2 + 20x - 10)/(50x^3 * -5 + \\sqrt{3})
+(\\sqrt{49 + x^2 * 2y})/(\\sqrt{13 + y})
 
-(\\sqrt{49 + x^2 * 2y})/(\\sqrt{13 + y})`;
+(5)/(63)
+multi word variable = \\pi
+\\text{multi word variable} = \\pi`;
 })();
